6f9ba9e17367f3bc4d52bba3c18e6867f994b3c9
yinnan-996 <1301142455@qq.com>
2018-09-29 18:06:22 +0800

    添加环境配置

---
+++ src/main/java/com/yinnan/learnboot/config/BeanConfiguration.java
@@ -0,0 +1,41 @@
+package com.yinnan.learnboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Description:  ---——require需求|ask问题|jira
+ * Design :  ----the  design about train of thought 设计思路
+ * User: yinnan
+ * Date: 2018/9/29
+ * Time: 16:25
+ * Email:yinnan@huli.com
+ *
+ * @author yinnan
+ * @since 1.0-SNAPSHOT
+ */
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    @Profile("dev")
+    public Runnable test1() {
+        System.out.println("开发环境使用的 Bean");
+        return () -> {};
+    }
+
+    @Bean
+    @Profile("test")
+    public Runnable test2() {
+        System.out.println("测试环境使用的 Bean");
+        return () -> {};
+    }
+
+    @Bean
+    @Profile("pro")
+    public Runnable test3() {
+        System.out.println("生成环境使用的 Bean");
+        return () -> {};
+    }
+}
