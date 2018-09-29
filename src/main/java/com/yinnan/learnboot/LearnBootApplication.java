4252d67abe6d8ac6087a53453cb0dfe7a23b7937
yinnan-996 <1301142455@qq.com>
2018-09-29 14:57:55 +0800

    修复编译问题

--- src/main/java/com/yinnan/learnboot/LearnBootApplication.java
+++ src/main/java/com/yinnan/learnboot/LearnBootApplication.java
@@ -9,5 +9,5 @@ public class LearnBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearnBootApplication.class, args);
         System.out.println("Hello");
-    }ß
+    }
 }
