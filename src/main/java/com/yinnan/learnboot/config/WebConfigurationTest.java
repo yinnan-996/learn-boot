12c5a9ed8caa2cc34e4378a3dbe651a0569c5444
yinnan-996 <1301142455@qq.com>
2018-09-30 18:09:05 +0800

    修改json版本

---
+++ src/main/java/com/yinnan/learnboot/config/WebConfigurationTest.java
@@ -0,0 +1,39 @@
+package com.yinnan.learnboot.config;
+
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Description:  ---——require需求|ask问题|jira
+ * Design :  ----the  design about train of thought 设计思路
+ * User: yinnan
+ * Date: 2018/9/30
+ * Time: 09:48
+ * Email:yinnan@huli.com
+ *
+ * @author yinnan
+ * @since 1.0-SNAPSHOT
+ */
+@Slf4j
+@Configuration
+@ConfigurationProperties(prefix = "com.yinnan", ignoreUnknownFields = false)
+@PropertySource("classpath:config/webConfig.properties")
+@Data
+@Component
+public class WebConfiguration {
+
+    private String userName;
+
+
+}
