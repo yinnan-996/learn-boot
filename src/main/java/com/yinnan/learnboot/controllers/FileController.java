c4ad1c293aec7c2ec416e79598c821f365a7b43e
yinnan-996 <1301142455@qq.com>
2018-09-30 19:04:46 +0800

    添加文件上传下载

---
+++ src/main/java/com/yinnan/learnboot/controllers/FileController.java
@@ -0,0 +1,59 @@
+package com.yinnan.learnboot.controllers;
+
+import com.yinnan.learnboot.model.FileInfo;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Description:  ---——require需求|ask问题|jira
+ * Design :  ----the  design about train of thought 设计思路
+ * User: yinnan
+ * Date: 2018/9/30
+ * Time: 19:01
+ * Email:yinnan@huli.com
+ *
+ * @author yinnan
+ * @since 1.0-SNAPSHOT
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    private String path = "/Users/yinnan/work/learn/filesTest/";
+
+    @PostMapping
+    public FileInfo upload(MultipartFile file) throws Exception {
+
+        System.out.println(file.getName());
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+
+        File localFile = new File(path, file.getOriginalFilename());
+
+        file.transferTo(localFile);
+
+        return new FileInfo(localFile.getAbsolutePath());
+    }
+
+    @GetMapping("/{id}")
+    public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) {
+        try (InputStream inputStream = new FileInputStream(new File(path, id + ".jpg"));
+             OutputStream outputStream = response.getOutputStream();) {
+
+            response.setContentType("application/x-download");
+            response.addHeader("Content-Disposition", "attachment;filename=" + id + ".jpg");
+
+            IOUtils.copy(inputStream, outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
